828fa939b86d73303ec7526d4d52d21045d69acb
Gustavo Martínez <gustavo.martinez@gire.com>
2018-05-06 23:34:01 -0300

    Se agrega Swagger-UI al proyecto, el documento se escribe a mano

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Dependencias de Terceros
 const express = require('express');
-const bodyParser = require('body-parser');
-const Constantes = require('./utils/constantes');
-const ClienteController = require("./controllers/cliente.controller");
+const bodyParser = require('body-parser');            // Para parsear el body de las peticiones http
+const swaggerUi = require("swagger-ui-express");
 const mongoose = require("mongoose");
 
+// Dependencias Propias
+const Constantes = require('./utils/constantes');
+const ClienteController = require("./controllers/cliente.controller");
+const swaggerDocument = require("./swagger.json");
+
+// Conexion a la BBDD
 mongoose.connect(Constantes.BBDD_URL);
 
 const app = express();
@@ -11,5 +17,6 @@ const app = express();
 app.use(bodyParser.json());                           // para peticiones application/json
 app.use(bodyParser.urlencoded({extended: true}));     // para que acepte codificacion UTF-8
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/cliente", ClienteController);
 app.listen(Constantes.PORT, Constantes.HOST);
